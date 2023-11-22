c077d3eae0513143a99525d1bb2a1d4244f9c50e
Alexandre Dantas <alexandredantas690@outlook.com>
2023-11-21 22:58:17 -0300

    Atualização da máscara da senha

--- RateMusic.cpp
+++ RateMusic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 #include "sqlite3.h"
 #include "UsuarioControle.cpp"
 #include "Menu.h"
@@ -19,7 +20,8 @@ int hashingString(std::string str) {
 int main() {
     string email, senha; // são variáveis que armazenam as informações
     // do usuário após validação de login e cadastro.
-    string loginEmail, loginSenha; // são variáveis que armazenam informações
+    int digito; // será usado para criar o efeito de senha asterisco na tela
+    string loginEmail, loginSenha = ""; // são variáveis que armazenam informações
     // do usuário sem validação de login e cadastro.
     setlocale(LC_ALL, "portuguese");
     UsuarioControle controleUsuario;
@@ -31,11 +33,25 @@ int main() {
                 printf("Digite o seu email: ");
                 cin >> loginEmail;
                 printf("Digite a sua senha: ");
-                cin >> loginSenha;
+                digito = getch();
+                while (digito != 13) { //13 = carriage return (comando de iniciar nova linha)
+                    if (digito != 8) { // 8 = backspace 
+                        printf("*");
+                        loginSenha += (char) digito;
+                    }
+                    else {
+                        printf("\b \b"); // \b irá apagar qualquer caractere na linha
+                        // com espaço em branco
+                        if (loginSenha.length()) {
+                            loginSenha.resize(loginSenha.length() - 1);
+                        }
+                    }
+                    digito = getch();
+                }
                 if (controleUsuario.login(loginEmail, loginSenha)) {
                     email = loginEmail;
                     senha = loginSenha;
-                    puts("Login realizado com sucesso!");
+                    puts("\nLogin realizado com sucesso!");
                 }
                 else puts("Falha ao efetuar o login!");
                 break;
